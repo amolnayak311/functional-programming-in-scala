60e6170e13373155a4e25cd315cfcfb3c7f4f9b8
Amol Nayak <amolnayak311@gmail.com>
2016-12-08 23:28:14 +0530

    Ex 5.14

--- src/chapter5/Unfold.scala
+++ src/chapter5/Unfold.scala
@@ -3,7 +3,7 @@ package chapter5
 import chapter5.Stream._
 object Unfold {
   
-  //Implement Exercise 5.11, 5.12, 5.13 (tests, implementation in StreamImpl)
+  //Implement Exercise 5.11, 5.12, 5.13, 5.14
   
   /**
    * 
@@ -69,6 +69,27 @@ object Unfold {
   /**
    * 
    */
+  def zipAll1[A, B](s1: Stream[A], s2: Stream[B]):Stream[(Option[A], Option[B])] = unfold((s1, s2)) {
+    case (Cons(h1, t1), Cons(h2, t2)) => Some(((Some(h1()), Some(h2())), (t1(), t2())))
+    case (Cons(h1, t1), Empty) => Some(((Some(h1()), None), (t1(), Empty)))
+    case (Empty, Cons(h1, t1)) => Some(((None, Some(h1())), (Empty, t1())))
+    case (Empty, Empty) => None
+  }
+  
+  /**
+   * 
+   */
+  def startsWith[A](s: Stream[A], pre: Stream[A]):Boolean = zipAll1(s, pre).foldRight(true){
+    case ((Some(a), Some(b)), state) => if(state) a == b else state
+    case ((_, None), state) => state
+    case _ => false
+  }
+  
+  
+  
+  /**
+   * 
+   */
   def main(args: Array[String]): Unit = {
     assert(from(1).take(5).toList == List(1, 2, 3, 4, 5))
     assert(constant('a').take(5).toList == List('a', 'a', 'a', 'a', 'a'))
@@ -91,6 +112,22 @@ object Unfold {
     assert(zipWith(Empty: Stream[Int], Stream(1, 2, 3))(_ + _).toList == Nil)
     assert(zipWith(Stream(1, 2, 3), Empty)(_ + _).toList == Nil)
     assert(zipWith(Stream(1, 2, 3), Stream(1, 2, 3))(_ + _).toList == List(2, 4, 6))
+   
+    assert(zipAll1(Stream(1, 2, 3), Stream(1, 2)).toList == 
+      List((Some(1), Some(1)), (Some(2), Some(2)), (Some(3), None)))
+    assert(zipAll1(Stream(1, 2), Stream(1, 2, 3)).toList == 
+      List((Some(1), Some(1)), (Some(2), Some(2)), (None, Some(3))))
+      
+    assert(zipAll1(Stream(1, 2), Stream(1, 2)).toList == 
+      List((Some(1), Some(1)), (Some(2), Some(2))))
+      
+    assert(zipAll1(Empty, Empty).toList == Nil)
+    
+    assert(startsWith(Stream(1, 2, 3), Stream(1, 2)))
+    assert(!startsWith(Stream(1, 2), Stream(1, 2, 3)))
+    assert(startsWith(Stream(1, 2), Empty))
+    assert(startsWith(Stream(1, 2), Stream(1, 2)))
+    assert(startsWith(Empty, Empty))
     
     println("All tests successful")
   }
