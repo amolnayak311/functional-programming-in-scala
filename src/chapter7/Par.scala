4017de48c956760625aebc96774b840b933aebb9
Amol Nayak <amolnayak311@gmail.com>
2016-12-24 23:46:00 +0530

    Updated Par

--- src/chapter7/Par.scala
+++ src/chapter7/Par.scala
@@ -1,26 +1,55 @@
 package chapter7
 
-trait Par[A] {
-  
-    /**
-     * 
-     */
-    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
-    
-}
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.Callable
+
 
 object Par {
   
+    type Par[A] = ExecutorService => Future[A]
+    
+    case class UnitFuture[A](get: A) extends Future[A] {
+      /**
+       * 
+       */
+       def isDone = true
+       
+       /**
+        * 
+        */
+       def get(timeout: Long, units: TimeUnit) = get
+       
+       /**
+        * 
+        */
+       def isCancelled = false
+       
+       /**
+        * 
+        */
+       def cancel(evenIfRunning: Boolean): Boolean = false
+    }
+  
     /**
      * 
      */
-    def unit[A](a: A): Par[A] = ???
+    def unit[A](a: A): Par[A] = _ => new UnitFuture(a)
     
     
     /**
-     * 
+     * Spawns two threads, first one is when we call es.submit
+     * second is invoked for the a instance passed
      */
-    def fork[A](a : => Par[A]): Par[A] = ???
+    def fork[A](a : => Par[A]): Par[A] = es => {
+      es.submit(new Callable[A] {
+        /**
+         * 
+         */
+        def call: A = a(es).get
+      })
+    }
     
     
     /**
@@ -29,13 +58,25 @@ object Par {
     def lazyUnit[A](a : => A): Par[A] = fork(unit(a))
     
     
-    
+    /**
+     * 
+     */
+    def map2[A,B,C](a: Par[A], b: Par[B], timeout : Long = -1, unit: TimeUnit = TimeUnit.MILLISECONDS)(f: (A,B) => C): Par[C] = 
+    es => {
+      //Implementation that doesn't honor the timeouts
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+      
+      
     
     
     /**
      * 
      */
-    def get[A](p : Par[A]): A = ???
+    def run[A](es: ExecutorService)(p : Par[A]): Future[A] = p(es)
+    
         
   
 }
